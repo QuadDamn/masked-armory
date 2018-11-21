aa2a2a3b261f9fa359e36d48818888767f02a299
Shane Jeffery <paypal@khaccounts.net>
2018-11-21 12:55:12 -0800

    Finish up the Create Profile page, minus error handling.

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,36 +1,7 @@
-//homepage data inputs
-$(document).ready(function(){
-    $(".usa").click(function(event){
-		event.preventDefault();
-		$(".data2").hide();
-        $(".data1").fadeIn();
-    });
-	$(".euro").click(function(event){
-		event.preventDefault();
-		$(".data1").hide();
-        $(".data2").fadeIn();
-    });
-});
-
-
-//Friendly menu
 function openNav() {
-    document.getElementById("mynav").classList.add('mynav2');
+  document.getElementById("mynav").classList.add('mynav2');
 }
 
 function closeNav() {
-    document.getElementById("mynav").classList.remove('mynav2');
+  document.getElementById("mynav").classList.remove('mynav2');
 }
-
-//Awesomplete server name
-var input = document.getElementById("usaservers");
-new Awesomplete(input, {
-	list: ["mod_jk", "mod_lisp", "mod_mono", "mod_parrot", "mod_perl", "mod_php", "mod_proxy"]
-});
-
-var input2 = document.getElementById("euroservers");
-new Awesomplete(input2, {
-	list: ["mod_jk", "mod_lisp", "mod_mono", "mod_parrot", "mod_perl", "mod_php", "mod_proxy"]
-});
-
-
--- app/src/components/CreateProfile.js
+++ app/src/components/CreateProfile.js
@@ -1,94 +1,199 @@
 import React, {Component, Fragment} from 'react';
+import {Link} from 'react-router-dom';
+import Awesomplete from 'awesomplete';
 import {getUsServerList, getEuServerList} from "../utils/api";
+import $ from 'jquery';
 
 class CreateProfile extends Component {
+  state = {
+    region: 'us',
+    usServerList: [],
+    euServerList: [],
+    characterName: '',
+    serverName: '',
+    submissionError: ''
+  };
 
-    state = {
-        region: 'us',
-        usServerList: {},
-        euServerList: {},
-        characterName: '',
-        serverName: ''
-    };
+  /**
+   * Getting the US and EU server lists and setting them to the component state.
+   * Outside of that, we are also initializing the Awesompletes for both regions.
+   *
+   * @returns {Promise<void>}
+   */
+  async componentDidMount() {
+    const usServers = await getUsServerList();
+    const euServers = await getEuServerList();
 
-    async componentDidMount() {
-        const usServers = await getUsServerList();
-        const euServers = await getEuServerList();
+    this.setState({
+      usServerList: usServers,
+      euServerList: euServers
+    });
 
-        this.setState({
-            usServerList: usServers,
-            euServerList: euServers
-        })
+    this.initAwesompletes();
+    this.handleFlagSelection({}, 'us');
+  }
+
+  /**
+   * Handling the initialization of both Awesomplete, one for US and one for EU servers.
+   */
+  initAwesompletes() {
+    const usServerInput = document.getElementById('usServers');
+    const euServerInput = document.getElementById('euServers');
+
+    new Awesomplete(usServerInput, {
+      minChars: 2,
+      maxItems: 5,
+      replace: this.handleServerNameSelectionReplace(usServerInput),
+      list: this.state.usServerList
+    });
+
+    new Awesomplete(euServerInput, {
+      minChars: 2,
+      maxItems: 5,
+      replace: this.handleServerNameSelectionReplace(euServerInput),
+      list: this.state.euServerList
+    });
+  }
+
+  /**
+   * Handling the submission for the profile creation.
+   *
+   * @param event
+   */
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(this.state.region);
+    console.log(this.state.serverName);
+    console.log(this.state.characterName);
+  };
+
+  /**
+   * Handling the region flag change event.
+   *
+   * @param event
+   * @param region
+   */
+  handleFlagSelection = (event, region) => {
+    this.setState({
+      serverName: '',
+      region
+    });
+
+    // The reason for the use of jQuery here is to use the fadeIn functionality.
+    const usInputContainer = $('#usInputContainer');
+    const euInputContainer = $('#euInputContainer');
+
+    if (region === 'us') {
+      usInputContainer.fadeIn();
+      euInputContainer.hide();
+    } else if (region === 'eu') {
+      usInputContainer.hide();
+      euInputContainer.fadeIn();
     }
+  };
 
-    initAwesomplete() {
+  /**
+   * Handling the character name input change.
+   *
+   * @param event
+   */
+  handleCharacterNameChange = (event) => {
+    this.setState({
+      characterName: event.target.value
+    });
+  };
 
-    }
+  /**
+   * Handling the server name selection change from the Awesomplete.
+   *
+   * @param input
+   * @returns {Function}
+   */
+  handleServerNameSelectionReplace = (input) => {
+    return ((item) => {
+      this.setState({
+        serverName: item.value
+      });
+    })
+  };
 
-    handleSubmit = (event) => {
+  /**
+   * Handling the input change for the server name in the event that the user enters it in by hand
+   * and doesn't use the Awesomplete.
+   *
+   * @param event
+   */
+  handleServerNameChange = (event) => {
+    this.setState({
+      serverName: event.target.value
+    });
+  };
 
-    };
-
-    handleFlagSelection = (event) => {
-
-    };
-
-    handleCharacterNameChange = (event) => {
-        this.setState({
-           characterName: event.target.value
-        });
-    };
-
-    render() {
-        return (
-            <Fragment>
-                <div className="container-fluid home">
-                    <div className="container">
-                        <div className="logo"><img src="images/logo.png" alt=""/></div>
-                        <div className="site-description">
-                        <p>Masked Armory is the most well known anonymous World of Warcraft (WoW) profile source in the Real
-                            Money Trading (RMT) market. We hide your character and server name so that you can be protected
-                            when you are attempting to buy, sell, or trade a WoW account! <span className="bold">Come create your Masked Armory profile today in just a matter of seconds!</span>
-                        </p>
-                        </div>
-                        <div className="select"> Select Your Region</div>
-                        <div className="region"><a href="#">
-                            <div className="usa" />
-                        </a> <a href="#">
-                            <div className="euro" />
-                        </a></div>
-                        <div className="popup data1">
-                            <div className="form-group">
-                                <form>
-                                    <input className="form-control" placeholder="Your Server Name" id="usServers" />
-                                    <input type="text" className="form-control" placeholder="Your Character Name" onChange={(event) => this.handleCharacterNameChange(event) } />
-                                    <button className="select" type="submit">Create Armory Profile</button>
-                                </form>
-                            </div>
-                        </div>
-                        <div className="popup data2">
-                            <div className="form-group">
-                                <form>
-                                    <input className="form-control" placeholder="Your Server Name" id="euServers" />
-                                    <input type="text" className="form-control" placeholder="Your Character Name" onChange={(event) => this.handleCharacterNameChange(event) } />
-                                        <button className="select" type="submit">Create Armory Profile</button>
-                                </form>
-                            </div>
-                        </div>
-                        <div className="footer">
-                            <ul>
-                                <li><a href="#">Buy Sell WoW Accounts</a></li>
-                                <li className="none">|</li>
-                                <li><a href="#">Buy High End Elite Premium WoW Accounts</a></li>
-                                <li className="none">|</li>
-                                <li><a href="#">Sell High End Elite Premium WoW Accounts</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <div className="container-fluid home">
+          <div className="container">
+            <div className="logo"><img src="images/logo.png" alt=""/></div>
+            <div className="site-description">
+              <p>Masked Armory has been around since 2007 and has become the most well-known anonymous armory profile source for
+                World of Warcraft.  We aim to keep things simple and just ask for your character's region, server, and name and
+                we do the rest of the heavy lifting.  Our aim is to provide you a profile link that will keep you protected
+                when you are attempting to buy, sell, or trade your World of Wacraft account.
+              </p>
+            </div>
+            <div className="select"> Select Your Region</div>
+            <div className="region">
+              <Link to="/" onClick={(event) => this.handleFlagSelection(event, 'us')}><div className="us"/></Link>
+              <Link to="/" onClick={(event) => this.handleFlagSelection(event, 'eu')}><div className="eu"/></Link>
+            </div>
+            <div className="popup" id="usInputContainer">
+              <div className="form-group">
+                <form>
+                  <input
+                    value={this.state.serverName}
+                    className='form-control'
+                    id="usServers"
+                    onChange={(event) => this.handleServerNameChange(event)}
+                    placeholder="Server Name"
+                  />
+                  <input type="text" className="form-control" placeholder="Character Name"
+                         onChange={(event) => this.handleCharacterNameChange(event)}/>
+                  <button className="select" type="submit" style={{marginTop: "10px"}} onClick={(event) => this.handleSubmit(event)}>Create Armory Profile</button>
+                </form>
+              </div>
+            </div>
+            <div className="popup" id="euInputContainer">
+              <div className="form-group">
+                <form>
+                  <input
+                    value={this.state.serverName}
+                    className='form-control'
+                    id="euServers"
+                    onChange={(event) => this.handleServerNameChange(event)}
+                    placeholder="Server Name"
+                  />
+                  <input type="text" className="form-control" placeholder="Character Name"
+                         onChange={(event) => this.handleCharacterNameChange(event)}/>
+                  <button className="select" type="submit" style={{marginTop: "10px"}} onClick={(event) => this.handleSubmit(event)}>Create Armory Profile</button>
+                </form>
+              </div>
+            </div>
+            <div className="footer">
+              <ul>
+                <li><Link to='https://www.khaccounts.net'>Buy Sell WoW Accounts</Link></li>
+                <li className="none">|</li>
+                <li><Link to='https://www.khaccounts.net/buy-wow-accounts'>Buy High End Elite Premium WoW Accounts</Link></li>
+                <li className="none">|</li>
+                <li><Link to='https://www.khaccounts.net/sell-wow-accounts'>Sell High End Elite Premium WoW Accounts</Link></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    )
+  }
 }
 
 export default CreateProfile;
